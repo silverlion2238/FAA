ed15490521aa5b33c04affbe891e68056270035e
silverlion3328 <silverlion3328@gmail.com>
2025-03-22 22:40:32 +0100

    Changed LLM provider to Gemini API. Muted TTS when there are no matching voices.

--- lib/pages/chatbot.dart
+++ lib/pages/chatbot.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_ai_toolkit/flutter_ai_toolkit.dart';
 import 'package:flutter_application_0_0_5/models/language_model.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter_application_0_0_5/main.dart';
 import 'package:flutter_application_0_0_5/pages/settings.dart';
 import 'package:flutter_application_0_0_5/data/data.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_application_0_0_5/data/language_data.dart';
 import 'package:flutter_application_0_0_5/functions/http_functions.dart';
 
@@ -19,367 +22,49 @@ class ChatScreen extends StatefulWidget {
 }
 
 class ChatScreenState extends State<ChatScreen> {
-  final List<ChatMessage> _messages = <ChatMessage>[];
-  final TextEditingController _textController = TextEditingController();
 
-  List tempList = [];
-  String userKey = '';
-  String conversationID = '';
-  bool isConnected = false;
-
-  Future<bool> _checkConnection() async {
-    String checkConnection = await checkConnectivity();
-    
-    if (checkConnection == 'connected') {
-      print('data initialized');
-      isConnected = true;
-      return true;
-    } else {
-      //print('cant access internet');
-      if (mounted) {
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text('Connection Error'),
-            content: Text('Cannot access the internet. Please check your connection and try again.'),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text('OK'),
-              ),
-            ],
-          ),
-        );
-      }
-      return false;
-    }
-  }
- 
-  void _initializeData() async {
-    tempList = await ChatDataProvider().initializeData();
-    userKey = tempList[0];
-    conversationID = tempList[1];
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    
-    _checkConnection().then(
-      (isConnected) {
-        if (isConnected) {
-          print('initializing data');
-          _initializeData();
-        } else {
-          //print('initialization aborted');
-          if (mounted) {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text('Initialization Aborted'),
-                content: Text('Data initialization was aborted due to no internet connection.'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text('OK'),
-                  ),
-                ],
-              ),
-            );
-          }
-        }
-      }
+  ActionButtonStyle chatbotButtonStyle(context) {
+    return ActionButtonStyle(
+      iconDecoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        shape: BoxShape.circle,
+      ),
+      iconColor: Theme.of(context).colorScheme.onPrimary,
     );
   }
 
-
-  
-
-
-  /*
-  String userKey = '';
-  String conversationID = '';
-
-  createUser().then((_) {
-      createConversation(userKey);
-    });
-  */
-
-  void _handleSubmitted(String text,) async {
+  @override
+  Widget build(BuildContext context) {
     final locale = Provider.of<LanguageModel>(context, listen: false).locale.languageCode;
-      _textController.clear();
-
-
-
-    ChatMessage message = ChatMessage(
-      text: text,
-      isUser: true,
-    );
-    
-    setState(() {
-      _messages.insert(0, message);
-      }
-    );
-
-
-    //check internet connection
-    
-
-    // Send message to Botpress API
-    try {
-      var post = await http.post(
-        Uri.parse('https://chat.botpress.cloud/17157902-f630-4a68-b45a-b126fcbff509/messages'), // Replace with your Botpress API URL
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-          'x-user-key': userKey,
-        },
-        body: jsonEncode({
-          'payload': {
-            'text': message.text,
-            'type': 'text',
-          },
-          'conversationId': conversationID,
-        }),
-      );
-    
-
-      if (post.statusCode == 200) {
-        // Botpress API call successful
-      } else {
-        if (mounted) {
-          showDialog(
-            context: context,
-            builder: (context) => AlertDialog(
-              title: Text('Error'),
-              content: Text('Failed to send message to Botpress: ${post.statusCode}\nResponse body: ${post.body}'),
-              actions: [
-                TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text('OK'),
-                ),
-              ],
-            ),
-          );
-        }
-      }
-
-
-
-
-      String botReply = jsonDecode(post.body)['message']['payload']['text'];
-
-
-      while (botReply == jsonDecode(post.body)['message']['payload']['text']){
-        var response = await http.get(
-          Uri.parse('https://chat.botpress.cloud/17157902-f630-4a68-b45a-b126fcbff509/conversations/$conversationID/messages'), // Replace with your Botpress API URL
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-            'x-user-key': userKey,
-          },
-        );
-
-        if (response.statusCode == 200) {
-          // Parse the response and display the bot's reply
-          Map<String, dynamic> data;
-          print('Response body: ${response.body}');
-          try {
-            data = jsonDecode(response.body);
-          } catch (e) {
-            if (mounted) {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text('Error'),
-                content: Text('Error decoding response body: $e'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text('OK'),
-                  ),
-                ],
-              ),
-            );
-          }
-            return;
-          }
-
-
-          print(response.body);
-          print(response.request);
-          botReply = data['messages'][0]['payload']['text'];
-
-
-          // Check if the bot's reply is the same as the user's message
-          if (botReply == jsonDecode(post.body)['message']['payload']['text']) {
-            await Future.delayed(Duration(seconds: 1));
-          } else {
-            ChatMessage botMessage = ChatMessage(
-              text: botReply,
-              isUser: false,
-            );
-            setState(() {
-              _messages.insert(0, botMessage);
-              }
-            );
-            FlutterTtsSingleton.instance.speak(botReply);
-          }
-        } else {
-          // Handle error
-          if (mounted) {
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(
-                title: Text('Error'),
-                content: Text('Failed to get messages from Botpress: ${response.statusCode}\nResponse body: ${response.body}'),
-                actions: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text('OK'),
-                  ),
-                ],
-              ),
-            );
-          }
-        }
-      }
-    } catch (e) {
-      if (mounted) {
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text('Error'),
-            content: Text('Error sending message to Botpress: $e'),
-            actions: [
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: Text('OK'),
-              ),
-            ],
-          ),
-        );
-      }
-      return;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final dataProvider = Provider.of<ChatDataProvider>(context);
-    final locale = Provider.of<LanguageModel>(context).locale.languageCode;
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Flexible(
-            child: ListView.builder(
-              padding: EdgeInsets.all(8.0),
-              reverse: true,
-              itemBuilder: (_, int index) => _messages[index],
-              itemCount: _messages.length,
-            ),
-          ),
-          Divider(height: 1.0),
-          Container(
-            decoration: BoxDecoration(color: Theme.of(context).cardColor),
-            child: _buildTextComposer(locale),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildTextComposer(String locale) {
-    return IconTheme(
-      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
-      child: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Row(
-          children: <Widget>[
-            Flexible(
-              child: TextField(
-                controller: _textController,
-                onSubmitted: (text) {
-                  if (isConnected) {
-                  _handleSubmitted(text);
-                    } else {
-                    showDialog(
-                      context: context, 
-                      builder: (context) => AlertDialog(
-                        title: Text(translations[locale]!['wrongInput']!),
-                        content: Text(translations[locale]!['noResults']!),
-                        actions: [
-                          TextButton(onPressed: () => Navigator.pop(context), child: Text(translations[locale]!['ok']!)),
-                        ],
-                      )
-                    );
-                  }
-                },
-                decoration:
-                    InputDecoration.collapsed(
-                      hintText: translations[locale]!['sendMessage'] ?? "Send a message",
-                      hintStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface.withAlpha(150),),
-                    ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 4.0),
-              child: IconButton(
-                icon: Icon(Icons.send),
-                onPressed: () {
-                  if (isConnected) {
-                    _handleSubmitted(_textController.text);
-                  } else {
-                    showDialog(
-                      context: context, 
-                      builder: (context) => AlertDialog(
-                        title: Text(translations[locale]!['wrongInput']!),
-                        content: Text(translations[locale]!['noResults']!),
-                        actions: [
-                          TextButton(onPressed: () => Navigator.pop(context), child: Text(translations[locale]!['ok']!)),
-                        ],
-                      )
-                    );
-                  }
-                },
-              ),
-            ),
-          ],
+      body: LlmChatView(
+        style: LlmChatViewStyle(
+          backgroundColor: Theme.of(context).colorScheme.surface,
+          addButtonStyle: chatbotButtonStyle(context),
+          recordButtonStyle: chatbotButtonStyle(context),
+          chatInputStyle: ChatInputStyle(
+            backgroundColor: Theme.of(context).colorScheme.surface,
+            hintText: translations[locale]?['Type a message'] ?? 'Type a message',
+            textStyle: TextStyle(color: Theme.of(context).colorScheme.onSurface),
+            decoration: BoxDecoration(
+              border: Border.all(),
+              borderRadius: BorderRadius.circular(10),
+            ),                
+          )
         ),
-      ),
-    );
-  }
-}
-
-class ChatMessage extends StatelessWidget {
-  ChatMessage({required this.text, required this.isUser});
-
-  final String text;
-  final bool isUser;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Container(
-            margin: const EdgeInsets.only(right: 16.0),
-            child: CircleAvatar(child: Text(isUser ? 'U' : 'B')),
+        provider: GeminiProvider(
+          model: GenerativeModel(
+            model: 'gemini-1.5-flash',  
+            apiKey: 'AIzaSyDTfOaFnF9b0SBlHUbAIdgkh9ioYhBGBsQ',
           ),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(isUser ? 'You' : 'Bot',
-                    style: TextStyle(color: Theme.of(context).colorScheme.onSecondary)),
-                Container(
-                  margin: const EdgeInsets.only(top: 5.0),
-                  child: Text(text),
-                ),
-              ],
-            ),
+          chatGenerationConfig: GenerationConfig(
+            temperature: 0,
+            topK: 40,
+            topP: 0.95,
+            maxOutputTokens: 8192,
+            responseMimeType: 'text/plain',
           ),
-        ],
+        ),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,17 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import file_selector_macos
+import firebase_app_check
+import firebase_auth
 import firebase_core
 import flutter_tts
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAppCheckPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAppCheckPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
 }
